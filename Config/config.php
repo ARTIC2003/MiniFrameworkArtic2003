ef3d4d264eced1bfabea4badca78fca2d9ad0c39
Alvaro Noel Ocampo Zeledón <alvaronoelocampozeledon2003@gmail.com>
2022-05-07 19:12:45 -0600

    Subida total

---
+++ Config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+# URL
+	const BASE_URL = "http://localhost/ARTICMI2003/";
+	const LIBS = "Libraries/";
+	const VIEWS = "Views/";
+
+?>
---
+++ Views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ARTIC-BOT</title>
+</head>
+<body>
+	<h1>Welcome to artic-bot</h1>
+</body>
+</html>
